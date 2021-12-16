5bfd16791fffc549e1c5a313195663b7c9de88c5
AmrHE <amr.hassan.emam@gmail.com>
2021-12-16 22:57:39 +0200

    build users reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,1 +1,15 @@
 //REDUCERS ARE PURE FUNCTIONS
+import { GET_USERS } from "../actions/types";
+
+export default function users(state = {}, action) {
+	switch (action.type) {
+		case GET_USERS:
+			return {
+				...state,
+				...action.users,
+			};
+
+		default:
+			return state;
+	}
+}
