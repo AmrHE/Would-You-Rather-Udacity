4497d2cd9138e2e8362b0aaf397faafb56b25de8
AmrHE <amr.hassan.emam@gmail.com>
2021-12-16 23:01:38 +0200

    build questions reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,7 +1,15 @@
 //REDUCERS ARE PURE FUNCTIONS
-
 import { GET_QUESTIONS } from "../actions/types";
 
-const initialState = {
-	questions: [],
-};
+export default function questions(state = {}, action) {
+	switch (action.type) {
+		case GET_QUESTIONS:
+			return {
+				...state,
+				...action.questions,
+			};
+
+		default:
+			return state;
+	}
+}
