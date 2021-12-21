d2d013a8265a576430e4149c336656a1ca29d6c7
AmrHE <amr.hassan.emam@gmail.com>
2021-12-21 13:42:54 +0200

    updated the App Component

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,24 @@ import { handleInitialData } from "../actions/shared";
 import { getQuestions } from "../actions/questions";
 import { getUsers } from "../actions/users";
 import { connect } from "react-redux";
-import LoadingBar from "react-redux-loading";
+// import LoadingBar from "react-redux-loading";
 import Login from "./login/Login";
-import Nav from "./navbar/Nav";
+// import Nav from "./navbar/Nav";
 import Home from "./Home/Home";
 import LoadingOverlay from "react-loading-overlay";
 import Layout from "./Layout";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AddQuestion from "./addQuestion/AddQuestion";
 import Leaderboard from "./leaderboard/Leaderboard";
 import Question from "./question/Question";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./NotFound";
 
 export class App extends Component {
 	state = { loading: true };
 
 	render() {
-		// console.log("AuthedUser: ", this.props.authUser);
+		console.log("AuthedUser: ", this.props.authUser);
 		return (
 			<LoadingOverlay
 				active={this.state.loading}
@@ -27,34 +28,45 @@ export class App extends Component {
 				text="Loading, Please Wait..."
 			>
 				<div className="App">
-					{/* <LoadingBar /> */}
 					{this.props.authUser ? (
 						<Layout>
-							<Route path="/" exact={true} component={Home}></Route>
-							<Route path="/add" exact={true} component={AddQuestion}></Route>
-							<Route
-								path="/leaderboard"
-								exact={true}
-								component={Leaderboard}
-							></Route>
-							<Route
-								path="/questions/:id"
-								exact={true}
-								component={Question}
-							></Route>
+							<Switch>
+								<Route path="/" exact={true} component={Home} />
+								<Route path="/add" exact={true} component={AddQuestion} />
+								<Route
+									path="/leaderboard"
+									exact={true}
+									component={Leaderboard}
+								/>
+								<Route
+									path="/questions/:id"
+									exact={true}
+									component={Question}
+								/>
+								<Route path="*" component={NotFound} />
+							</Switch>
 						</Layout>
 					) : (
-						<Login />
+						// <Login />
+						<Switch>
+							<Route exact={true} path="/" component={Login} />
+							<Route exact={true} path="/add" component={Login} />
+							<Route exact={true} path="/leaderboard" component={Login} />
+							<Route exact={true} path="/questions/:id" component={Login} />
+							<Route path="*" component={NotFound} />
+						</Switch>
 					)}
 				</div>
 			</LoadingOverlay>
 		);
 	}
+
 	handleInitialData() {
 		this.props.getUsers();
 		this.props.getQuestions();
 		this.setState({ loading: false });
 	}
+
 	componentDidMount() {
 		this.handleInitialData();
 	}
